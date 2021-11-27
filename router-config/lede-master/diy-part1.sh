0257d5f0f8cc09284a6a8a8d6df40836f4ae59cf
razif73 <raziffahmi2890@gmail.com>
2021-11-28 06:46:14 +0700

    Update diy-part1.sh

--- router-config/lede-master/diy-part1.sh
+++ router-config/lede-master/diy-part1.sh
@@ -3,16 +3,12 @@
 # https://github.com/ophub/amlogic-s9xxx-openwrt
 # Description: Automatically Build OpenWrt for Amlogic S9xxx STB
 # Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
-# Source code repository: https://github.com/coolsnowwolf/lede / Branch: master
+# Source code repository: https://github.com/Lienol/openwrt / Branch: openwrt-21.02
 #========================================================================================================================
 
-# Uncomment a feed source
-# sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
-# sed -i 's/\"#src-git\"/\"src-git\"/g' feeds.conf.default
-
 # Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# sed -i 's/src-git packages/#src-git packages/g' ./feeds.conf.default
+# sed -i '$a src-git packages https://github.com/immortalwrt/packages.git;openwrt-21.02' feeds.conf.default
 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
-
